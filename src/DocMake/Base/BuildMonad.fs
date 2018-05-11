003042688cee9bfb4c18b88be36a18bc3e862b61
stephentetley <stephen.tetley@gmail.com>
2018-05-11 15:09:29 +0100

    Added state to the BuildMonad so it can generate tempfile names.

--- src/DocMake/Base/BuildMonad.fs
+++ src/DocMake/Base/BuildMonad.fs
@@ -29,43 +29,52 @@ type Answer<'a> =
 type Env = 
     { WorkingDirectory: string }
 
+
+
 type State = 
-    { NameIndex: int }
+    { MakeName: int -> string
+      NameIndex: int }
+
+let private incrNameIndex (st:State) : State = 
+    let i = st.NameIndex in {st with NameIndex = i + 1 }
 
 
 type BuildMonad<'res,'a> = 
-    private BuildMonad of ((Env * 'res) -> StringBuilder -> Answer<'a>)
+    private BuildMonad of ((Env * 'res) -> StringBuilder -> State -> (State * Answer<'a>))
 
-let inline private apply1 (ma : BuildMonad<'res,'a>) (handle:Env * 'res) (sbuf:StringBuilder) : Answer<'a> = 
-    let (BuildMonad f) = ma in f handle sbuf
+let inline private apply1 (ma : BuildMonad<'res,'a>) (handle:Env * 'res) (sbuf:StringBuilder) (st:State) : State *  Answer<'a> = 
+    let (BuildMonad f) = ma in f handle sbuf st
 
-let inline private apply1Ex (ma : BuildMonad<'res,'a>) (handle:Env * 'res) (sbuf:StringBuilder) : 'a = 
-    let (BuildMonad f) = ma
-    match f handle sbuf with
-    | Err msg -> failwith msg
-    | Ok a -> a
-
-
+//let inline private apply1Ex (ma : BuildMonad<'res,'a>) (handle:Env * 'res) (sbuf:StringBuilder) : 'a = 
+//    let (BuildMonad f) = ma
+//    match f handle sbuf with
+//    | Err msg -> failwith msg
+//    | Ok a -> a
 
 
-let inline private unitM (x:'a) : BuildMonad<'res,'a> = BuildMonad (fun _ _ -> Ok x)
+
+
+let inline private unitM (x:'a) : BuildMonad<'res,'a> = 
+    BuildMonad (fun _ _ st -> st, Ok x)
 
 
 let inline private bindM (ma:BuildMonad<'res,'a>) (f : 'a -> BuildMonad<'res,'b>) : BuildMonad<'res,'b> =
-    BuildMonad <| fun res sbuf -> 
-        match apply1 ma res sbuf with
-        | Err s -> Err s
-        | Ok a -> apply1 (f a) res sbuf
+    BuildMonad <| fun res sbuf st0 -> 
+        let st1, ans = apply1 ma res sbuf st0
+        match ans with
+        | Err s -> (st1, Err s)
+        | Ok a -> apply1 (f a) res sbuf st1
 
 
 let inline private altM (ma:BuildMonad<'res,'a>) (mb:BuildMonad<'res,'a>) : BuildMonad<'res,'a> =
-    BuildMonad <| fun res sbuf -> 
+    BuildMonad <| fun res sbuf st0 -> 
         let sbuf2 = new StringBuilder() 
-        match apply1 ma res sbuf2 with
-        | Err s -> apply1 ma res sbuf
+        let st1, ans = apply1 ma res sbuf2 st0
+        match ans with 
+        | Err s -> apply1 ma res sbuf st1
         | Ok a -> 
             sbuf.AppendLine (sbuf2.ToString()) |> ignore
-            Ok a
+            st1, Ok a
 
 type BuildMonadBuilder() = 
     member self.Return x = unitM x
@@ -77,123 +86,147 @@ let (buildMonad:BuildMonadBuilder) = new BuildMonadBuilder()
 
 // Common monadic operations
 let fmapM (fn:'a -> 'b) (ma:BuildMonad<'res,'a>) : BuildMonad<'res,'b> = 
-    BuildMonad <| fun res sbuf ->
-        match apply1 ma res sbuf with
-        | Err msg -> Err msg
-        | Ok a -> Ok (fn a)
+    BuildMonad <| fun res sbuf st0 ->
+        let (st1,ans) =  apply1 ma res sbuf st0 
+        match ans with
+        | Err msg -> (st1, Err msg)
+        | Ok a -> (st1, Ok <| fn a)
 
 
 let mapM (fn:'a -> BuildMonad<'res,'b>) (xs:'a list) : BuildMonad<'res,'b list> =
-    BuildMonad <| fun res sbuf -> 
-        let rec work ac ys = 
+    BuildMonad <| fun res sbuf state -> 
+        let rec work ac (st0:State) ys = 
             match ys with
-            | [] -> Ok <| List.rev ac
+            | [] -> (st0, Ok <| List.rev ac)
             | z :: zs ->
-                match apply1 (fn z) res sbuf with
-                | Err msg -> Err msg 
-                | Ok a -> work (a::ac) zs
-        work [] xs
+                let (st1,ans) = apply1 (fn z) res sbuf st0
+                match ans with
+                | Err msg -> st1, Err msg 
+                | Ok a -> work (a::ac) st1 zs
+        work [] state xs
 
 let forM (xs:'a list) (fn:'a -> BuildMonad<'res,'b>) : BuildMonad<'res,'b list> = 
     mapM fn xs
 
 
 let mapMz (fn:'a -> BuildMonad<'res,'b>) (xs:'a list) : BuildMonad<'res,unit> =
-    BuildMonad <| fun res sbuf -> 
-        let rec work ys = 
+    BuildMonad <| fun res sbuf state -> 
+        let rec work st0 ys = 
             match ys with
-            | [] -> Ok ()
+            | [] -> (st0, Ok ())
             | z :: zs ->
-                match apply1 (fn z) res sbuf with
-                | Err msg -> Err msg 
-                | Ok _ -> work zs
-        work xs
+                let (st1,ans) = apply1 (fn z) res sbuf st0
+                match ans with
+                | Err msg -> (st1, Err msg)
+                | Ok _ -> work st1 zs
+        work state xs
 
 
 let forMz (xs:'a list) (fn:'a -> BuildMonad<'res,'b>) : BuildMonad<'res,unit> = 
     mapMz fn xs
 
-let traverseM (fn: 'a -> BuildMonad<'res,'b>) (source:seq<'a>) : BuildMonad<'res,seq<'b>> = 
-    BuildMonad <| fun res sbuf ->
-        try 
-            Seq.map (fun a -> apply1Ex (fn a) res sbuf) source |> Ok
-        with 
-        | ex -> Err (ex.Message)
 
 
-let traverseMz (fn: 'a -> BuildMonad<'res,'b>) (source:seq<'a>) : BuildMonad<'res,unit> = 
-    BuildMonad <| fun res sbuf ->
-        try 
-            Seq.fold (fun ac x -> 
-                        let ans  = apply1Ex (fn x) res sbuf in ac) 
-                     (Ok ())
-                     source 
-        with
-        | ex -> Err (ex.Message)
+//let traverseM (fn: 'a -> BuildMonad<'res,'b>) (source:seq<'a>) : BuildMonad<'res,seq<'b>> = 
+//    BuildMonad <| fun res sbuf state ->
+//        Seq.mapFold (fun ac x -> 
+//                        match ac with
+//                        
+//                        let (st1,ans  = apply1 (fn x) res sbuf in ac) 
+//                     (GoodTrav state ())
+//                     source 
+
+
+//let traverseMz (fn: 'a -> BuildMonad<'res,'b>) (source:seq<'a>) : BuildMonad<'res,unit> = 
+//    BuildMonad <| fun res sbuf ->
+//        try 
+//            Seq.fold (fun ac x -> 
+//                        let ans  = apply1Ex (fn x) res sbuf in ac) 
+//                     (Ok ())
+//                     source 
+//        with
+//        | ex -> Err (ex.Message)
 
 
 
 let mapiM (fn:int -> 'a -> BuildMonad<'res,'b>) (xs:'a list) : BuildMonad<'res,'b list> = 
-    BuildMonad <| fun res sbuf -> 
-        let rec work ac ix ys = 
+    BuildMonad <| fun res sbuf state -> 
+        let rec work ac ix st0 ys = 
             match ys with
-            | [] -> Ok <| List.rev ac
+            | [] -> (st0, Ok <| List.rev ac)
             | z :: zs ->
-                match apply1 (fn ix z) res sbuf with
-                | Err msg -> Err msg 
-                | Ok a -> work (a::ac) (ix+1) zs
-        work [] 0 xs
+                let (st1,ans) = apply1 (fn ix z) res sbuf st0
+                match ans with
+                | Err msg -> (st1, Err msg)
+                | Ok a -> work (a::ac) (ix+1) st1 zs
+        work [] 0 state xs
 
 let mapiMz (fn:int -> 'a -> BuildMonad<'res,'b>) (xs:'a list) : BuildMonad<'res,unit> =
-    BuildMonad <| fun res sbuf -> 
-        let rec work ix ys = 
+    BuildMonad <| fun res sbuf state -> 
+        let rec work ix st0 ys = 
             match ys with
-            | [] -> Ok ()
+            | [] -> (st0, Ok ())
             | z :: zs ->
-                match apply1 (fn ix z) res sbuf with
-                | Err msg -> Err msg 
-                | Ok _ -> work (ix+1) zs
-        work 0 xs
+                let (st1,ans) =  apply1 (fn ix z) res sbuf st0
+                match ans with
+                | Err msg -> (st1, Err msg)
+                | Ok _ -> work (ix+1) st1 zs
+        work 0 state xs
 
 
 // BuildMonad operations
-let runBuildMonad (env:Env) (handle:'res) (ma:BuildMonad<'res,'a>) : string * Answer<'a>= 
+let runBuildMonad (env:Env) (handle:'res) (stateZero:State) (ma:BuildMonad<'res,'a>) : State * string * Answer<'a>= 
     let sb = new StringBuilder () 
     match ma with 
-    | BuildMonad fn -> let ans = fn (env,handle) sb in (sb.ToString(), ans)
+    | BuildMonad fn -> let (s1,ans) = fn (env,handle) sb stateZero in (s1, sb.ToString(), ans)
 
 
 
 
 let launch (handle:'res1) (ma:BuildMonad<'res1,'a>) : BuildMonad<'res2,'a> = 
-    BuildMonad <| fun (env,_) sbuf -> 
-        let (s,ans) = runBuildMonad env handle ma
-        sbuf.AppendLine(s) |> ignore
-        ans
+    BuildMonad <| fun (env,_) sbuf st0 -> 
+        let (st1,msg,ans) = runBuildMonad env handle st0 ma
+        sbuf.AppendLine(msg) |> ignore
+        (st1,ans)
 
 
 let tell (msg:string) : BuildMonad<'res,unit> = 
-    BuildMonad <| fun _ sbuf -> 
+    BuildMonad <| fun _ sbuf st0 -> 
         sbuf.Append(msg) |> ignore
-        Ok ()
+        (st0, Ok ())
 
 
 let tellLine (msg:string) : BuildMonad<'res,unit> = 
-    BuildMonad <| fun _ sbuf -> 
+    BuildMonad <| fun _ sbuf st0 -> 
         sbuf.AppendLine(msg) |> ignore
-        Ok ()
+        (st0, Ok ())
 
 
 /// Note unit param to avoid value restriction.
 let askU () : BuildMonad<'res,'res> = 
-    BuildMonad <| fun (_,res) _ -> Ok res
+    BuildMonad <| fun (_,res) _ st0 -> (st0, Ok res)
 
 let asksU (project:'res -> 'a) : BuildMonad<'res,'a> = 
-    BuildMonad <| fun (_,res) _ -> Ok (project res)
+    BuildMonad <| fun (_,res) _ st0 -> (st0, Ok (project res))
 
 let localU (modify:'res -> 'res) (ma:BuildMonad<'res,'a>) : BuildMonad<'res,'a> = 
-    BuildMonad <| fun (env,res) sbuf -> apply1 ma (env,modify res) sbuf
+    BuildMonad <| fun (env,res) sbuf st0 -> apply1 ma (env, modify res) sbuf st0
 
 
 
+/// The MakeName function is in State rather than Env, but we only provide an API
+///  run it within a context (cf. the Reader monad's local), rather than reset it 
+/// imperatively.
 
+let localState (modify:State -> State) (ma:BuildMonad<'res,'a>): BuildMonad<'res,'a> = 
+    BuildMonad <| fun (env,res) sbuf st0 -> 
+        let (_,ans) = apply1 ma (env,res) sbuf (modify st0)
+        (st0,ans)
+
+let withNameGen (namer:int -> string) (ma:BuildMonad<'res,'a>): BuildMonad<'res,'a> =  
+    localState (fun s -> { s with  MakeName = namer; NameIndex = 1}) ma
+
+let fileNameGen () : BuildMonad<'res, string> = 
+    BuildMonad <| fun _ _  st0 -> 
+        let i = st0.NameIndex
+        (incrNameIndex st0, Ok <| st0.MakeName i)
