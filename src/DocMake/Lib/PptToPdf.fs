06053512b2fccb21922d1bdce13b2f606d8f66a6
stephentetley <stephen.tetley@gmail.com>
2018-05-21 10:58:51 +0100

    Added tasks for Powerpoint-to-Pdf and Excel-to-Pdf that run in the DocBuild monad.

---
+++ src/DocMake/Lib/PptToPdf.fs
@@ -0,0 +1,41 @@
+﻿module DocMake.Lib.PptToPdf
+
+
+// Open at .Interop rather than .Word then the Powerpoint API has to be qualified
+open Microsoft.Office.Interop
+
+
+open DocMake.Base.Common
+open DocMake.Base.OfficeUtils
+open DocMake.Builder.BuildMonad
+open DocMake.Builder.Basis
+open DocMake.Builder.Builders
+
+
+let private getOutputName (pptDoc:PowerPointDoc) : PowerPointBuild<string> =
+    executeIO <| fun () -> 
+        System.IO.Path.ChangeExtension(pptDoc.DocumentPath, "pdf")
+    
+
+
+let private process1 (inpath:string) (outpath:string) (quality:DocMakePrintQuality) (app:PowerPoint.Application) : unit = 
+    try 
+        let prez = app.Presentations.Open(inpath)
+        prez.ExportAsFixedFormat (Path = outpath,
+                                    FixedFormatType = PowerPoint.PpFixedFormatType.ppFixedFormatTypePDF,
+                                    Intent = powerpointPrintQuality quality) 
+        prez.Close();
+    with
+    | ex -> printfn "PptToPdf - Some error occured for %s - '%s'" inpath ex.Message
+
+
+
+
+let pptToPdf (pptDoc:PowerPointDoc) : PowerPointBuild<PdfDoc> =
+    buildMonad { 
+        let! (app:PowerPoint.Application) = askU ()
+        let! outPath = getOutputName pptDoc
+        let! quality = asksEnv (fun s -> s.PrintQuality)
+        let _ =  process1 pptDoc.DocumentPath outPath quality app
+        return (makeDocument outPath)
+    }
---
+++ src/DocMake/Lib/XlsToPdf.fs
@@ -0,0 +1,52 @@
+﻿module DocMake.Lib.XlsToPdf
+
+
+// Open at .Interop rather than .Word then the Excel API has to be qualified
+open Microsoft.Office.Interop
+
+
+open DocMake.Base.Common
+open DocMake.Base.OfficeUtils
+open DocMake.Builder.BuildMonad
+open DocMake.Builder.Basis
+open DocMake.Builder.Builders
+
+
+let private getOutputName (xlsDoc:ExcelDoc) : ExcelBuild<string> =
+    executeIO <| fun () -> 
+        System.IO.Path.ChangeExtension(xlsDoc.DocumentPath, "pdf")
+    
+
+
+let private process1 (inpath:string) (outpath:string) (quality:DocMakePrintQuality) (fitWidth:bool) (app:Excel.Application)  : unit = 
+    try 
+        let workbook : Excel.Workbook = app.Workbooks.Open(inpath)
+        if fitWidth then 
+            workbook.Sheets 
+                |> Seq.cast<Excel.Worksheet>
+                |> Seq.iter (fun (sheet:Excel.Worksheet) -> 
+                    sheet.PageSetup.Zoom <- false
+                    sheet.PageSetup.FitToPagesWide <- 1)
+        else ()
+
+        workbook.ExportAsFixedFormat (Type=Excel.XlFixedFormatType.xlTypePDF,
+                                         Filename=outpath,
+                                         IncludeDocProperties=true,
+                                         Quality = excelPrintQuality quality
+                                         )
+        workbook.Close (SaveChanges = false)
+    with
+    | ex -> printfn "%s" ex.Message
+
+
+
+
+
+let xlsToPdf (fitWidth:bool) (xlsDoc:ExcelDoc) : ExcelBuild<PdfDoc> =
+    buildMonad { 
+        let! (app:Excel.Application) = askU ()
+        let! outPath = getOutputName xlsDoc
+        let! quality = asksEnv (fun s -> s.PrintQuality)
+        let _ =  process1 xlsDoc.DocumentPath outPath quality fitWidth app
+        return (makeDocument outPath)
+    }
