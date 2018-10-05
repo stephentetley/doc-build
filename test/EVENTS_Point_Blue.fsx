096e113bb58387c2873bab7fe04a4cda80c91cbc
stephentetley <stephen.tetley@gmail.com>
2018-10-05 13:48:31 +0100

    Work on EVENTS_Point_Blue script.

--- test/EVENTS_Point_Blue.fsx
+++ test/EVENTS_Point_Blue.fsx
@@ -64,6 +64,8 @@ open DocMake.Tasks
 open Proprietry
 
 
+let _outputRoot     = @"G:\work\Projects\events2\point-blue\output"
+
 
 // ****************************************************************************
 // Generate output from a work list
@@ -89,11 +91,12 @@ let cover (siteName:string) (phase:string) : FullBuild<PdfDoc> =
         // Look somewhere we won't find it...
         | _ -> @"G:\work\Projects\events2\point-blue\cover-sheets"
 
-    let pattern1 = underscoreName(siteName)
-
+    let pattern1 = sprintf "%s*" (underscoreName siteName)
+    printfn "Pattern: '%s'" pattern1
     buildMonad { 
         match tryFindExactlyOneMatchingFile pattern1 coversLocation with
         | Some doc -> 
+            printfn "doc is '%s'" doc
             let! d1 = docToPdf (makeDocument doc)
             return d1
         | None -> 
@@ -102,7 +105,7 @@ let cover (siteName:string) (phase:string) : FullBuild<PdfDoc> =
 
 let workSheet (siteName:string) : FullBuild<PdfDoc> = 
     let formsLoc =  @"G:\work\Projects\events2\point-blue\pb-commissioning-forms"
-    let pattern1 = underscoreName(siteName)
+    let pattern1 = sprintf "%s*" (underscoreName siteName)
     buildMonad { 
         match tryFindExactlyOneMatchingFile pattern1 formsLoc with
         | Some doc -> 
@@ -118,9 +121,88 @@ let makeFinal (siteName:string) (phase:string) (cover:PdfDoc) (worksheet:PdfDoc)
         | "PHASE ONE" -> "T0877"
         | "PHASE TWO" -> "T0942"
         | _ -> "Unknown"
+    let finalName = sprintf "%s %s Hawkeye Asset Replacement.pdf" (safeName siteName) scheme
     buildMonad { 
         let pdfs : PdfDoc list = [cover; worksheet]
-        let finalName = sprintf "%s %s Hawkeye Asset Replacement.pdf" phase (safeName siteName)
+        printfn "Final: '%s'" finalName
         let! (final:PdfDoc) = makePdf finalName <| pdfConcat pdfs
         return final   
     }
+
+
+let build1 (row:InputRow) : FullBuild<PdfDoc> = 
+    let phaseFolder = 
+        match row.Phase with
+        | "PHASE ONE" -> "phase1"
+        | "PHASE TWO" -> "phase2"
+        | _ -> "phase2"
+    localSubDirectory phaseFolder
+        <| buildMonad {
+            let! d1 = cover row.``Site Name`` row.Phase
+            let! d2 = workSheet row.``Site Name``
+            let! final = makeFinal row.``Site Name`` row.Phase d1 d2
+            return final
+        }
+
+ 
+
+let makeUploadRow (name:SiteName) (sai:SAINumber) (projCode:string) : UploadRow = 
+    let docTitle = 
+        sprintf "%s %s Hawkeye Asset Replacement" (name.Replace("/", " ")) projCode
+    let docName = 
+        sprintf "%s %s Hawkeye Asset Replacement.pdf" (underscoreName name) projCode
+
+    UploadTable.Row(assetName = name,
+                    assetReference = sai,
+                    projectName = "RTU Asset Replacement",
+                    projectCode = projCode,
+                    title = docTitle,
+                    category = "O & M Manuals",
+                    referenceNumber = projCode, 
+                    revision = "1",
+                    documentName = docName,
+                    documentDate = standardDocumentDate (),
+                    sheetVolume = "" )
+
+
+let uploadReceipts (rows:InputRow list): FullBuild<unit> = 
+    let siteNames (phase:string) : string list = 
+        List.map (fun (row:InputRow) -> row.``Site Name``)
+            <| List.filter (fun (row:InputRow) -> row.Phase = phase) rows
+
+    let uploadHelper (projCode:string) = 
+        { new IUploadHelper
+          with member this.MakeUploadRow name sai = makeUploadRow name sai projCode }
+    
+    buildMonad { 
+        do! localSubDirectory "phase1" (makeUploadForm  (uploadHelper "T0877") (siteNames "PHASE ONE"))
+        do! localSubDirectory "phase2" (makeUploadForm  (uploadHelper "T0942") (siteNames "PHASE TWO"))
+        return ()
+    }       
+
+
+let buildScript () : FullBuild<unit> = 
+    buildMonad { 
+        let siteList = rows () |> List.take 4
+        let count = siteList.Length
+        do! foriMz siteList <| fun ix row -> 
+                printfn "Site %i of %i (%s):" (ix+1) count row.``Site Name``
+                fmapM ignore <| build1 row
+
+        do! uploadReceipts siteList
+        return ()
+    }
+
+
+let main () : unit = 
+    let env = 
+        { WorkingDirectory = _outputRoot
+          PrintQuality = PrintQuality.PqScreen
+          PdfQuality = PdfPrintQuality.PdfScreen }
+    
+    let appConfig : FullBuildConfig = 
+        { GhostscriptPath = @"C:\programs\gs\gs9.15\bin\gswin64c.exe"
+          PdftkPath = @"C:\programs\PDFtk Server\bin\pdftk.exe"
+          PandocPath = @"pandoc" } 
+
+    runFullBuild env appConfig (buildScript ())
