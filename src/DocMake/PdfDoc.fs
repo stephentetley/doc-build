d8707643c49918b2d288a5b21f91b9a046d779e3
stephentetley <stephen.tetley@gmail.com>
2018-11-15 11:00:51 +0000

    Added an experiment that builds concatenated Pdfs in the style of a pretty print library. Potentially this could provide a better API (simpler, following a commonly used pattern) than what we currently have.

---
+++ src/DocMake/PdfDoc.fs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Stephen Tetley 2018
+// License: BSD 3 Clause
+
+
+module DocMake.PdfDoc
+
+open DocMake.Base.Common
+open DocMake.Base.RunProcess
+
+/// Concat PDFs with Ghostscript
+/// We favour Ghostscript because it lets us lower the print 
+/// quality (and reduce the file size).
+
+ 
+let private gsOptions (quality:PdfPrintQuality) : string =
+    match ghostscriptPrintSetting quality with
+    | "" -> @"-dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite"
+    | ss -> sprintf @"-dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dPDFSETTINGS=%s" ss
+
+let private gsOutputFile (fileName:string) : string = 
+    sprintf "-sOutputFile=\"%s\"" fileName
+
+let private gsInputFile (fileName:string) : string = sprintf "\"%s\"" fileName
+
+
+/// Apparently we cannot send multiline commands to execProcess.
+let private makeGsCommand (quality:PdfPrintQuality) (outputFile:string) (inputFiles: string list) : string = 
+    let line1 = gsOptions quality + " " + gsOutputFile outputFile
+    let rest = List.map gsInputFile inputFiles
+    String.concat " " (line1 :: rest)
+
+
+
+type PdfPath = string
+
+type GhostscriptOptions = 
+    { WorkingDirectory: string 
+      GhostscriptExe: string 
+      PrintQuality: PdfPrintQuality         // Make this Ghostscript specific?
+    }
+
+/// A PdfDoc is actually a list of Pdf files that are rendered 
+/// to a single document with Ghostscript.
+/// This means we have monodial concatenation.
+type PdfDoc = 
+    val Documents : PdfPath list
+
+    new () = 
+        { Documents = [] }
+
+    new (filePath:PdfPath) = 
+        { Documents = [filePath] }
+
+    internal new (paths:PdfPath list ) = 
+        { Documents = paths }
+
+
+    member internal v.Body 
+        with get() : PdfPath list = v.Documents
+
+    member v.Save(options: GhostscriptOptions, outputPath: string) : unit = 
+        let command = makeGsCommand options.PrintQuality outputPath v.Body
+        match executeProcess options.WorkingDirectory options.GhostscriptExe command with
+        | Choice2Of2 i when i = 0 -> ()
+        | Choice2Of2 i -> 
+            printfn "%s" command; failwithf "PdfDoc.Save - error code %i" i
+        | Choice1Of2 msg -> 
+            printfn "%s" command; failwithf "PdfDoc.Save - '%s'" msg
+
+
+
+let emptyPdf : PdfDoc = new PdfDoc ()
+
+let pdfDoc (path:PdfPath) : PdfDoc = new PdfDoc (filePath = path)
+
+let (^^) (x:PdfDoc) (y:PdfDoc) : PdfDoc = 
+    new PdfDoc(paths = x.Body @ y.Body)
+
+let concat (docs:PdfDoc list) = 
+    let xs = List.concat (List.map (fun (d:PdfDoc) -> d.Body) docs)
+    new PdfDoc(paths = xs)
---
+++ test/TempPdfDoc.fsx
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Stephen Tetley 2018
+// License: BSD 3 Clause
+
+#load "..\src\DocMake\Base\Common.fs"
+#load "..\src\DocMake\Base\RunProcess.fs"
+#load "..\src\DocMake\PdfDoc.fs"
+open DocMake.Base.Common
+open DocMake.PdfDoc
+
+let demo01 () = 
+    let working = System.IO.Path.Combine(__SOURCE_DIRECTORY__, "..", "data")
+    let options = 
+        { WorkingDirectory = working
+        ; GhostscriptExe = @"C:\programs\gs\gs9.15\bin\gswin64c.exe" 
+        ; PrintQuality = PdfPrintQuality.PdfScreen
+        }
+    let p1 = System.IO.Path.Combine(working, "One.pdf")
+    let p2 = System.IO.Path.Combine(working, "Two.pdf")
+    let p3 = System.IO.Path.Combine(working, "Three.pdf")
+    let d1 = concat <| List.map pdfDoc [p1;p2;p3]
+    d1.Save(options, "concat.pdf")
+
+
