6a4688972cfc1d860db104ad3c485647017786ee
stephentetley <stephen.tetley@gmail.com>
2019-01-12 22:59:23 +0000

    Added a mockup of how we can have largely typesafe extensibility for different application resources (like Word, Excel, etc.).

---
+++ test/Temp02.fsx
@@ -0,0 +1,79 @@
+﻿// Copyright (c) Stephen Tetley 2018,2019
+// License: BSD 3 Clause
+
+#r "netstandard"
+
+type HasWordAppKey =
+    abstract WordAppKey : string
+
+type Phantom1 = class end
+
+
+type MyConfig =  
+    | MyConfig of unit
+    override x.ToString() = "*MY_CONFIG*"
+            
+    interface HasWordAppKey with
+        member x.WordAppKey = "WordApp"
+
+let test01 () = 
+    let x = MyConfig () in (x :> HasWordAppKey).WordAppKey
+
+let quoteOfficeKey (mystery:HasWordAppKey) : string = 
+    sprintf "'%s'" mystery.WordAppKey
+
+let quoteOfficeKeyTwo (x:HasWordAppKey) : string = 
+    sprintf "'%s'" x.WordAppKey
+
+
+
+let test02 () = 
+    let x = MyConfig () in quoteOfficeKey (x :> HasWordAppKey)
+
+
+
+let test03 () = 
+    let x = MyConfig () in quoteOfficeKeyTwo x
+
+
+let test04 () = 
+    let x = MyConfig () in (x :> HasWordAppKey).WordAppKey
+
+
+type UserResources<'reskey> = 
+     val private UserResources: Map<string,obj>
+     val private KeyStore: 'reskey
+     
+     new (reskey:'reskey, resources:Map<string,obj>) = 
+        { UserResources = resources
+          KeyStore = reskey }
+
+     member x.Map
+        with get() = x.UserResources
+
+     member x.Keymap
+        with get() = x.KeyStore
+
+type WordHandle = 
+    { WordApp : unit }
+
+type WordResources = 
+    | WordRes of unit
+    interface HasWordAppKey with
+        member x.WordAppKey = "WordApp"
+
+let MyResources : UserResources<WordResources> = 
+    let resources : WordResources = WordRes ()
+    let wordApp = { WordApp = () }
+    let wordKey = (resources :> HasWordAppKey).WordAppKey
+    new UserResources<WordResources> (resources, Map.ofList [ wordKey, wordApp :> obj ])
+
+let extractWord<'T when 'T :> HasWordAppKey> (resources:UserResources<'T>) : WordHandle = 
+    let x = resources.Keymap
+    let key = x.WordAppKey
+    let o:obj = Map.find key resources.Map
+    o :?> WordHandle
+
+let testImportant () = 
+    extractWord MyResources
+
